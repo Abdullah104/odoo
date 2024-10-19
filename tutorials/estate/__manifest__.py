e66f271ac3c70d6d0c2e994d6110bbd02ac5f19e
Abdullah Sharef <38993855+Abdullah104@users.noreply.github.com>
2024-10-19 13:12:19 +0300

    Chapter 5: Finally, Some UI To Play With

--- tutorials/estate/__manifest__.py
+++ tutorials/estate/__manifest__.py
@@ -3,6 +3,8 @@
   'license': 'LGPL-3',
   'application': True,
   'data': [
-    'security/ir.model.access.csv'
+    'security/ir.model.access.csv',
+    'views/estate_property_views.xml',
+    'views/estate_menus.xml'
   ]
 }
